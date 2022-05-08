93d7b2d6785fb0ac1394ec95d10bdd6cc15e54f3
Boris Edison <borisedison@gmail.com>
2022-05-08 15:30:54 +0530

    Customizing Tailwin>Production Bundle

---
+++ 10-Customiziing using Config/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["*"],
+  theme: {              //for screens we can't write in extend
+    screens: {
+      xsm: '500px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
+    },
+    extend: {
+      spacing: {
+        13: '3.25rem'
+      },
+      fontSize:{
+        '10xl': ['9rem', { lineHeight: '1.2'}],
+      },
+    },
+  },
+  plugins: [],
+}
